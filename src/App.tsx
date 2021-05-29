5012f7b20dc82f104b21ecb41b18b91e09817a52
Jacquit <jacquit.aja@onja.org>
2021-05-29 15:41:05 +0300

    creating the main screen

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainScreen from './MainScreen/index'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <div className='App'>
+      <MainScreen />
+    </div>
+  )
+}
+
+export default App
