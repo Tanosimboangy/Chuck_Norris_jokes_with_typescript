d7a73fd4dbe1438b7e8644ccc5243998c9e55569
Jacquit <jacquit.aja@onja.org>
2021-06-02 14:57:48 +0300

    adding the functionalities for downloading the file txt

--- src/containers/SaveJokes.tsx
+++ src/containers/SaveJokes.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useState, useContext, useEffect, useCallback } from 'react'
 import styled from 'styled-components'
 import Plus from '../images/plus.svg'
 import Minus from '../images/minus.svg'
@@ -15,6 +15,7 @@ interface Errorbtn {
 const ButtonsContainer = styled.div`
   width: 100%;
   display: flex;
+  position: relative;
   justify-content: space-between;
 `
 const CountingButtons = styled.div<Errorbtn>`
@@ -63,18 +64,72 @@ const SaveButtons = styled.button<IBtn>`
   background: ${(props) => (props.pay === 'true' ? '#34394f' : '#f5f6f8')};
   color: ${(props) => (props.pay === 'true' ? '#ffffff' : '#34394f')};
 `
+const ErrorText = styled.p`
+  color: #f39a9a;
+  font-weight: 600;
+  margin-top: 12px;
+  position: absolute;
+  bottom: -32px;
+  opacity: 0.6;
+  font-weight: 600;
+  line-height: 1.63;
+  letter-spacing: -0.52px;
+  text-align: left;
+}
+`
+
 const Joke: React.FC = () => {
   const {
-    fetchJoke,
     decreaseJoke,
     increaseJoke,
     numberOfJoke,
     selectedCategory,
-    impersonateName,
-    getRandomJoke,
+    firstName,
+    lastName,
   } = useContext(GlobalContext)
-  // const joke = await fetchJoke(`jokes/random?${numberOfJoke}&${selectedCategory}&${firstName}&${lastName}&${impersonateName}`
-  // return setRandomJoke(joke
+
+  const [jokes, setJokes] = useState([])
+
+  const namesFromInput = `firstName=${firstName}&lastName=${lastName}`
+
+  const url = `https://api.icndb.com/jokes/random/${numberOfJoke}?${namesFromInput}&${selectedCategory}`
+
+  const fetchMultiplesJokes = useCallback(async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setJokes(data.value)
+  }, [url, setJokes])
+
+  useEffect(() => {
+    fetchMultiplesJokes()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [numberOfJoke, firstName, lastName, fetchMultiplesJokes])
+
+  const saveJokeToDownload = () => {
+    fetchMultiplesJokes()
+
+    const item = jokes.map(
+      (joke: { id: number; joke: string; categories: string[] }) =>
+        '-' + joke.joke + '\r\n'
+    )
+    const textToPrint = item
+
+    const ConvertIntoBLOB = new Blob(textToPrint, { type: 'text/plain' })
+    const defaultFileName = 'savedjoke.txt'
+
+    let link = document.createElement('a')
+    link.download = defaultFileName
+
+    if (window.webkitURL !== null) {
+      link.href = window.webkitURL.createObjectURL(ConvertIntoBLOB)
+    } else {
+      link.href = window.URL.createObjectURL(ConvertIntoBLOB)
+      link.style.display = 'none'
+      document.body.appendChild(link)
+    }
+
+    link.click()
+  }
 
   return (
     <ButtonsContainer>
@@ -102,9 +157,12 @@ const Joke: React.FC = () => {
         type='button'
         pay={numberOfJoke > 0 ? 'true' : 'false'}
         disabled={numberOfJoke > 0 && numberOfJoke < 100 ? false : true}
-        onClick={getRandomJoke}>
+        onClick={saveJokeToDownload}>
         Save Jokes
       </SaveButtons>
+      {numberOfJoke > 100 && (
+        <ErrorText>You can pick a number from 1 to 100.</ErrorText>
+      )}
     </ButtonsContainer>
   )
 }
